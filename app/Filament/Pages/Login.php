7982233b7525a78efbe6dc054224d66bde3df421
Dave Mills <d.e.mills@stats4sd.org>
2026-07-08 15:48:27 +0100

    hide register link if registration is not available

--- app/Filament/Pages/Login.php
+++ app/Filament/Pages/Login.php
@@ -2,7 +2,21 @@
 
 namespace App\Filament\Pages;
 
+use App\Models\SiteSetting;
+use Illuminate\Contracts\Support\Htmlable;
+
 class Login extends \Filament\Auth\Pages\Login
 {
-    //
+    /**
+     * Hide the "or sign up for an account" link when registration is invite-only
+     * (i.e. open registration is disabled).
+     */
+    public function getSubheading(): string | Htmlable | null
+    {
+        if (! SiteSetting::instance()->open_registration) {
+            return null;
+        }
+
+        return parent::getSubheading();
+    }
 }
